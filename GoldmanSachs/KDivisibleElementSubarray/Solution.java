b5483a4538c82b781a46693c4d3846987f4e638f
sid13-d <shindesiddhesh13062003@gmail.com>
2024-01-05 19:55:07 +0530

    started JAN:2 Day::4 KDivisibleElementSubarray 10::solved 4::backlog

---
+++ GoldmanSachs/KDivisibleElementSubarray/Solution.java
@@ -0,0 +1,31 @@
+package GoldmanSachs.KDivisibleElementSubarray;
+
+// https://leetcode.com/problems/k-divisible-elements-subarrays/description/?source=submission-ac
+import java.util.*;
+
+class Solution {
+    public int countDistinct(int[] nums, int k, int p) {
+        Set<String> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int count =0;
+
+            StringBuilder sb = new StringBuilder();
+            for(int j=i; j<nums.length; j++) {
+
+                    if(nums[j]%p == 0) {
+                        count++;
+                    }
+
+                    if(count>k)
+                        break;
+                    
+                sb.append(nums[j]+"_");
+                set.add(sb.toString());
+            }
+
+        }
+
+        return set.size();
+    }
+}
